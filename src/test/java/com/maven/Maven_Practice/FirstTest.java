40ae5b6a62bc7b565c32a1738bcffec03642c8d6
Rajani <rajini.yalamarthy@gmail.com>
2019-02-04 09:28:34 -0700

    First commit to my maven repo

---
+++ src/test/java/com/maven/Maven_Practice/FirstTest.java
@@ -0,0 +1,12 @@
+package com.maven.Maven_Practice;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	@Test
+	void firstmethod()
+	{
+		System.out.println("firstmethod");
+	}
+
+}
---
+++ src/test/java/com/maven/Maven_Practice/SecondTest.java
@@ -0,0 +1,12 @@
+package com.maven.Maven_Practice;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	@Test
+	void secondmethod()
+	{
+		System.out.println("secondmenthod");
+	}
+
+}
